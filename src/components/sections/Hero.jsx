2599ce9136f1f09adca1663665215e8c8a5f75c4
aswanthnarayan <aswanthnarayanan@gmail.com>
2025-07-04 18:04:30 +0530

    resume and projects

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -128,7 +128,7 @@ const Hero = ({ img }) => {
       <StyledHeroContainer ismobile={isMobile}>
         <StyledHeroGrid ismobile={isMobile} container spacing={0}>
           <Grid item style={{ paddingLeft: "0 !important" }}>
-            <StyledHeroImage alt="Image of Maximilian Oberholtzer" src={img} />
+            <StyledHeroImage alt="Image of Aswanth CK" src={img} />
           </Grid>
           <StyledHeroText item>
             <StyledHeroTitle component="h1" variant="h2">
